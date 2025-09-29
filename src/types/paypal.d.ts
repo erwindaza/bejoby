d752ed4ffd0d6492d4eb39cfc1f802f78ea59704
Erwin Daza <erwin.daza@gmail.com>
2025-09-28 21:50:37 -0300

    updates3

--- src/types/paypal.d.ts
+++ src/types/paypal.d.ts
@@ -3,6 +3,30 @@ export {};
 
 declare global {
   interface Window {
-    paypal: any; // 👈 puedes tiparlo después, pero así se quita el error
+    paypal: {
+      Buttons: (options: {
+        createOrder: (
+          data: Record<string, unknown>,
+          actions: {
+            order: {
+              create: (input: {
+                purchase_units: { amount: { value: string } }[];
+              }) => Promise<string>;
+            };
+          }
+        ) => Promise<string>;
+        onApprove: (
+          data: { orderID: string },
+          actions: {
+            order: {
+              capture: () => Promise<{ id: string }>;
+            };
+          }
+        ) => Promise<void>;
+        onError?: (err: unknown) => void;
+      }) => {
+        render: (element: HTMLElement) => void;
+      };
+    };
   }
 }
